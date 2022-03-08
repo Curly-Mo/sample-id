ff8250b96be6898118198db4d19c2204b04f7b59
Colin Fahy <colinfahy@spotify.com>
2022-03-08 15:46:32 -0500

    formatting

--- sample_id/audio.py
+++ sample_id/audio.py
@@ -14,7 +14,7 @@ def cqtgram(y, sr, hop_length=512, octave_bins=24, n_octaves=8, fmin=40, percept
     specgram = np.abs(s_complex)
     if perceptual_weighting:
         freqs = librosa.cqt_frequencies(specgram.shape[0], fmin=fmin, bins_per_octave=octave_bins)
-        specgram = librosa.perceptual_weighting(specgram ** 2, freqs, ref=np.max)
+        specgram = librosa.perceptual_weighting(specgram**2, freqs, ref=np.max)
     else:
         specgram = librosa.amplitude_to_db(specgram, ref=np.max)
     return specgram
