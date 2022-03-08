c778a0f6105d83124a1657c3078d87b80cc41803
Colin Fahy <colinfahy@spotify.com>
2022-03-08 15:58:10 -0500

    update main

--- sample_id/main.py
+++ sample_id/main.py
@@ -2,7 +2,10 @@
 import logging
 from typing import Iterable
 
-from sample_id import ann, hough, io
+import numpy as np
+
+from sample_id import ann
+from sample_id.ann import annoy
 from sample_id.fingerprint import fingerprint
 from sample_id.io import Track
 
@@ -31,29 +34,27 @@ def train_keypoints(
     logger.info("Settings: {}".format(settings))
 
     spectrograms = {}
-    keypoints = []
-    descriptors = []
+    # keypoints = []
+    # descriptors = []
     model_tracks = {}
+    fingerprints = []
     for track in tracks:
         track_id = str(track)
         model_tracks[track_id] = track
-        fp = fingerprint.from_file(track.path, sr, track_id, settings)
-        if dedupe:
-            # Remove duplicate keypoints
-            # (important for ratio thresholding if source track
-            # has exact repeated segments)
-            fp.remove_similar_keypoints()
+        fp = fingerprint.from_file(track.path, track_id, sr, dedupe=dedupe, **settings)
+        fingerprints.extend(fp)
 
-        if save_spec:
-            path = save_spectrogram(fp.spectrogram, track_id, save)
-            spectrograms[track_id] = path
-        keypoints.extend(fp.keypoints)
-        descriptors.extend(fp.descriptors)
-    descriptors = np.vstack(descriptors)
-    matcher = ann.train_matcher(descriptors, algorithm=algorithm)
-    descriptors = None
-    model = Model(matcher, keypoints, settings, tracks=model_tracks)
-    model.spectrograms = spectrograms
-    if save:
-        save_model(model, save)
-    return model
+        # if save_spec:
+        #     path = save_spectrogram(fp.spectrogram, track_id, save)
+        #     spectrograms[track_id] = path
+        # keypoints.extend(fp.keypoints)
+        # descriptors.extend(fp.descriptors)
+    # descriptors = np.vstack(descriptors)
+    # matcher = annoy.AnnoyMatcher.create(descriptors, algorithm=algorithm)
+    matcher = annoy.AnnoyMatcher.from_fingerprints(fingerprints)
+    # descriptors = None
+    # model = Model(matcher, keypoints, settings, tracks=model_tracks)
+    # model.spectrograms = spectrograms
+    # if save:
+    #     save_model(model, save)
+    return matcher
